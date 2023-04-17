7368c4a66558a0e7876188c6cc6de16d80c52768
Jagrut-Khetalpar <khetalparjagrut@gmail.com>
2023-04-17 13:36:49 +0530

    Initial commit

---
+++ App/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from App.views import Predict
+from App.views import FileView
+from App.views import FileDeleteView
+
+# app_name = 'App'
+
+urlpatterns = [
+    url(r'^predict/$', Predict.as_view(), name='APIpredict'),
+    url(r'^upload/$', FileView.as_view(), name='APIupload'),
+    url(r'^delete/$', FileDeleteView.as_view(), name='APIdelete'),
+]
